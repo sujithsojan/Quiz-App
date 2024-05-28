249b8d3a5cded2f7acbbedd9b2b5358749f6146c
Sujith <sujithsojan@gmail.com>
2024-05-28 17:05:33 +0530

    Getting shuffled answers

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -17,33 +17,24 @@ class _QuestionState extends State<QuestionScreen> {
     final currentQuestion = questions[0];
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            currentQuestion.text,
-            style: const TextStyle(color: Colors.white),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Answers(
-            answerText: currentQuestion.answers[0],
-            onTap: () {},
-          ),
-          Answers(
-            answerText: currentQuestion.answers[1],
-            onTap: () {},
-          ),
-          Answers(
-            answerText: currentQuestion.answers[2],
-            onTap: () {},
-          ),
-          Answers(
-            answerText: currentQuestion.answers[3],
-            onTap: () {},
-          ),
-        ],
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: const TextStyle(color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ...currentQuestion.getShuffledAnswers().map((answer) => 
+            Answers(answerText: answer, onTap: () {})),
+          ],
+        ),
       ),
     );
   }
